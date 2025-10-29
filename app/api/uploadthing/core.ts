ed66650e5db69b60b50da21839c14f3c96a62766
Monu_2114 <monu.nanabala@gmail.com>
2025-10-29 14:53:23 +0530

    bug fix server error

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,4 +1,4 @@
-import { currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { UploadThingError } from "uploadthing/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
@@ -6,10 +6,10 @@ const f = createUploadthing();
 export const ourFileRouter = {
   pdfUploader: f({ pdf: { maxFileSize: "32MB" } })
     .middleware(async ({ req }) => {
-      const user = await currentUser();
+      const { userId } = await auth();
 
-      if (!user) throw new UploadThingError("Unauthorized");
-      return { userId: user.id };
+      if (!userId) throw new UploadThingError("Unauthorized");
+      return { userId };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       console.log("upload completed for user id", metadata.userId);
